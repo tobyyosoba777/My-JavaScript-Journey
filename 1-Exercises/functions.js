8c16220cc40381f7dff0087d8b73abe5be4f6f77
tobiloba <tobiosoba777@gmail.com>
2023-08-05 06:12:50 -0700

    updated

--- 1-Exercises/functions.js
+++ 1-Exercises/functions.js
@@ -1,6 +1,6 @@
 //Write a JavaScript Function to return the age of a person and it should take the year of birth
 function age_calculator(year) {
-    console.log("you are " + 2023 - Number (year));
+    console.log("you are " + 2023 - Number(year));
 }
 
-age_calculator(2010)
+age_calculator(Number(2010))
