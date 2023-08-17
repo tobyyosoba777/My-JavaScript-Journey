ec5865746691ce1e3360d1592f5d42fba1f79e83
tobiloba <tobiosoba777@gmail.com>
2023-08-17 15:55:05 -0700

    updated

--- Js-Basics/reusableFunctions.js
+++ Js-Basics/reusableFunctions.js
@@ -20,3 +20,31 @@ function operationSum(a,b,c) {
     console.log(a+c * (b));
 }
 operationSum(2,5,10); //Will follow BODMAS rule.
+
+
+
+
+
+
+
+//Global and Local variable
+
+var myGlobal = 10;
+
+function fun1() {
+    oopsGlobal = 5;
+}
+
+function fun2() {
+    var output = "";
+    if (typeof myGlobal != undefined) {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != undefined) {
+        output += "oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+
+}
+fun1();
+fun2();
