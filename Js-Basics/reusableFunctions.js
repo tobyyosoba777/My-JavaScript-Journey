97e3c6eddc4df4ca316497f7463fb935b33b1f0b
tobiloba <tobiosoba777@gmail.com>
2023-08-17 17:28:39 -0700

    updated

--- Js-Basics/reusableFunctions.js
+++ Js-Basics/reusableFunctions.js
@@ -59,3 +59,24 @@ function myLocalScope() {
 
 myLocalScope();
 // console.log(myVar);
+
+var myGlobal = 10;
+
+function fun1() {
+     oopsGlobal = 5;
+}
+
+function fun2() {
+    output = "";
+    if(typeof myGlobal != undefined) {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != undefined) {
+        output += "oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+}
+
+
+fun1();
+fun2();
