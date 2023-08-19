9c2f1338c6e233b394a587449716d9b42cbe768a
tobiloba <tobiosoba777@gmail.com>
2023-08-19 15:56:18 -0700

    Updated

---
+++ Js-Basics/Booleans.js
@@ -0,0 +1,44 @@
+number = 1;
+
+function returnBool(number) {
+    if (number == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(returnBool(1));
+
+
+
+
+function welcomeToBooleans() {
+    return true;
+}
+calledFunction =  welcomeToBooleans();
+console.log(calledFunction);
+
+
+
+
+function trueOrFalse(wasThatTrue) {
+    if (wasThatTrue) {
+        return "Yes that was true";
+    } else {
+        return "No that was false";
+    }
+}
+trueOrFalse(true);
+
+
+
+
+function trueFalse(wasThatTrue) {
+    if (wasThatTrue == true) {
+        return "Yes, true";
+    }
+    else {
+        return "Nah it was not true";
+    }
+}
+console.log(trueFalse(true));
