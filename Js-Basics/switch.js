9996b63fb32af6c6e34a4e4f730c5850b43666a2
tobiloba <tobiosoba777@gmail.com>
2023-08-27 13:40:17 -0700

    Updated

--- Js-Basics/switch.js
+++ Js-Basics/switch.js
@@ -1,1 +1,12 @@
 //Switch case in JavaScript
+//switch statement is an alternative for if else statements
+
+function caseInSwitch(val) {
+    var answer = "";
+    switch(val) {
+        case 1: 
+            answer = "alpha";
+        case 2:
+            answer = "beta"; 
+    }
+}
