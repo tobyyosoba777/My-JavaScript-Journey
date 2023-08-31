f040605f092197d14e419b068f3c5720d90f3975
tobiloba <tobiosoba777@gmail.com>
2023-08-31 03:18:18 -0700

    Updated

--- Js-Basics/switch.js
+++ Js-Basics/switch.js
@@ -6,14 +6,21 @@ function caseInSwitch(val) {
     switch(val) {
         case 1: 
             answer = "alpha";
+            break;
         case 2:
             answer = "beta";
+            break;
         case 3:
             answer = "gamma";
+            break;
 
         case 4:
             answer = "delta";
+            break;
     }
+
+    return answer;
+
 }
 
 function caseInSwitch(val) {
