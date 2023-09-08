230dab580d70a39c3116a209babf20cfb7726af7
tobiloba <tobiosoba777@gmail.com>
2023-09-08 15:30:26 -0700

    Updated

--- Js-Basics/switch.js
+++ Js-Basics/switch.js
@@ -88,7 +88,57 @@ function switchOfStuff(val) {
         case "c":
             answer = "cat";
             break;
+            default: //This also functions as the else keyword
+                answer = "only cases are a, b, c "
     }
     return answer;
 }
-console.log(switchOfStuff("a"));
+console.log(switchOfStuff(2));
+
+function sequentialSizes(val) {
+    var answer = "";
+    switch (val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High"
+            break
+    }
+    return answer;
+}
+console.log(sequentialSizes(5));
+
+
+
+
+
+function chainToSwitch(val) {
+    var answer = "";
+    switch (val) {
+        case "bob":
+            answer = "Marley";
+            break;
+        case 42:
+            answer = "The answer";
+            break;
+        case 1:
+            answer = "There is no #1";
+            break;
+        case 99:
+            answer = "Missed me by this much!";
+            break;
+        case 7:
+            answer = "Ate Nine"
+
+    }
+}
