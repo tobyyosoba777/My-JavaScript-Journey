be8712997330990e2b494e1afb82a7cdd5253c9d
tobiloba <tobiosoba777@gmail.com>
2023-09-10 14:53:26 -0700

    Updated

--- Projects/Digital Clock/script.js
+++ Projects/Digital Clock/script.js
@@ -1,24 +1,26 @@
-function displayTime(){
+function displayTime() {
     var date = new Date();
-    var hrs = date.getHours();
-    var minutes = date.getMinutes();
+    var hours = date.getHours();
+    var minute = date.getMinutes();
     var seconds = date.getSeconds();
     var session = document.getElementById('session');
 
-    if (hrs >= 12) {
+    if(hours >= 12) {
         session.innerHTML = 'PM';
     }
     else {
-        session.innerHTML = 'AM';
+        session.innerHTML = 'AM'
     }
 
-    if(hrs > 12) {
-        hrs = hrs - 12;
+    
+    if(hours > 12) {
+        hours = hours - 12;
     }
 
-    document.getElementById('hours').innerHTML = hrs;
-    document.getElementById('minute').innerHTML = minutes;
+
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minute').innerHTML = minute;
     document.getElementById('seconds').innerHTML = seconds;
-}
 
+}
 setInterval(displayTime, 10);
