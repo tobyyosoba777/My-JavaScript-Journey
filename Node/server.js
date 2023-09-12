6dc7af06254f883b64eb6247325ff60370265488
tobiloba <tobiosoba777@gmail.com>
2023-09-12 15:37:56 -0700

    updated

--- Node/server.js
+++ Node/server.js
@@ -1,9 +1,21 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, world!\n');
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello, world!\n');
+// });
+
+// server.listen(3008, '127.0.0.1');
+// console.log('Server running at http://127.0.0.1:3008/');
+
+
+
+
+var http = require('http');
+var server = http.createServer(function(request, response) {
+    console.log("got a requet!");
+    response.write("hi, its me");
+    response.end();
 });
 
-server.listen(3008, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:3008/');
+server.listen(3003);
