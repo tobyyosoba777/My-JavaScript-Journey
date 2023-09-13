a108dee55c167751461f7532b40c27a3440fe98a
tobiloba <tobiosoba777@gmail.com>
2023-09-13 09:21:14 -0700

    Updated

---
+++ 1-Exercises/programmersday.js
@@ -0,0 +1,35 @@
+//A function to check and display happy programmers day
+
+function happyProgrammersDay() {
+    const today = new Date();
+    const month = today.getMonth() + 1
+    return month === 9 && (today.getDate() === 12 || today.getDate() === 13);
+}
+
+if (happyProgrammersDay()) {
+  console.log("Happy Programmer's Day!");
+} else {
+  console.log("Today is not Programmer's Day.");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
