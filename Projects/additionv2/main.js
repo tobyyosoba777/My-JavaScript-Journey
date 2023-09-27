19968cf5828632139d3c28014198a4d0624df121
tobiloba <tobiosoba777@gmail.com>
2023-09-27 15:39:17 -0700

    Updated

--- Projects/additionv2/main.js
+++ Projects/additionv2/main.js
@@ -36,6 +36,16 @@ function divideNumbers() {
     }
 }
 
+function divideNumbers() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+
+    if(!isNaN(num1) && !isNaN(num2)) {
+        var sum = num1 / num2;
+        document.getElementById('result').textContent = sum;
+    }
+}
+
 function subtractNumbers() {
     var num1 = parseFloat(document.getElementById('num1').value);
     var num2 = parseFloat(document.getElementById('num2').value);
