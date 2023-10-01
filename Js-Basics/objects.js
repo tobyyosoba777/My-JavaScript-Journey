65e541e061b63d67a96af2afceb60f3e90ec55ba
tobiloba <tobiosoba777@gmail.com>
2023-10-01 15:33:07 -0700

    Updated

--- Js-Basics/objects.js
+++ Js-Basics/objects.js
@@ -431,3 +431,54 @@ var datarandom = {
 dataRandomCopy = JSON.parse(JSON.stringify(datarandom));
 console.log(dataRandomCopy);
 console.log(datarandom);
+
+var collection = {
+    "2548": {
+        "album": "Slippery When Wet",
+        "artist": "Bon Jovi",
+        "tracks": [
+            "let it rock",
+            "you give love a bad name"
+        ]
+    },
+    "2468": {
+        "album": "1999",
+        "artist": "Prince",
+        "tracks": [
+            "1999",
+            "Little Red Corvette"
+        ]
+    },
+    "1245": {
+        "artist": "Robert Palmer",
+        "tracks": [ ]
+    },
+    "5439": {
+        "album": "ABBA Gold"
+    }
+};
+
+//Keep a copy of collection for tests
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+console.log(collectionCopy)
+
+function updateRecords(id, prop, value) {
+    if (value === "") {
+        delete collection[id][prop]
+    }
+    else if (prop === "tracks") {
+        collection[id][prop] = collection[id][prop] || [];
+        collection[id][prop].push(value);
+    }
+    else {
+        collection[id][prop] = value;
+    }
+
+
+
+    return collection;
+}
+
+//Alter values below to test your code
+updateRecords(5439, "artist", "ABBA")
+console.log(updateRecords(5439, "artist", "ABBA"));
