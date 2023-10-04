993e9567d8e02a01ecdaa742f2e085a9ec4f96c5
tobiloba <tobiosoba777@gmail.com>
2023-10-04 14:59:00 -0700

    Updated

--- Js-Basics/objects.js
+++ Js-Basics/objects.js
@@ -494,6 +494,21 @@ function updateRecords(id, prop, value) {
     return collection;
 }
 
+function updateRecords(id, prop, value) {
+    if(value === "") {
+        delete [collection][id][prop];
+    }
+    else if(prop === "tracks") {
+        collection[id][prop] = collection[id][prop] || [];
+        collection[id][prop].push(value);
+    }
+    else {
+        collection[id][prop] = value;
+    }
+
+    return collection;
+}
+
 
 
 //Alter values below to test your code
