01a3eb5adafeaddaf655dd0466053e9341de50ac
tobiloba <tobiosoba777@gmail.com>
2023-10-23 06:48:53 -0700

    Updated

--- Js-Basics/loops.js
+++ Js-Basics/loops.js
@@ -72,3 +72,83 @@ while(j<8) {
     j++;
 }
 console.log(arry);
+
+var myArray = [];
+
+var i = 0;
+while (i<5) {
+    myArray.push(i);
+    i++;
+}
+console.log(myArray);
+
+var ourArr = [];
+for (var i = 0; i < 5; i++) {
+    ourArr.push(i);
+}
+console.log(ourArr);
+
+var newArr = [];
+for(var i = 1; i < 6; i++) {
+    newArr.push(i);
+}
+console.log(newArr);
+
+
+var testArray = [];
+for(i = 0; i < 10; i+=2) {
+    testArray.push(i);
+}
+console.log(testArray)
+
+var newTest = [];
+for(i = 0; i <= 30; i+=3) {
+    newTest.push(i);
+}
+console.log(newTest);
+
+var testSub = [];
+for(i = 0; i <= 100; i+=10) {
+    testSub.push(i);
+}
+console.log(testSub);
+
+var newOne = [];
+for(i = 100; i <= 500; i+=100) {
+    newOne.push(i);
+}
+console.log(newOne);
+
+var oddArray = [];
+for (var i = 1; i < 10; i+=2) {
+    oddArray.push(i);
+}
+console.log(oddArray);
+
+//Count backwards with a for loop
+
+var backArr = [];
+for (var i=5; i > 0; i--) {
+    backArr.push(i);
+}
+console.log(backArr);
+
+var backArr2 = []
+for (i = 10; i > 0; i -= 2) {
+    backArr2.push(i);
+}
+console.log(backArr2);
+
+var oddBack = []
+for (i=9; i>= 1; i -= 2) {
+    oddBack.push(i);
+}
+
+console.log(oddBack);
+
+var arrayy = [1,2,3,4,5,6,7,8,9,10];
+for (i in arrayy) {
+    console.log(i)
+}
+
+
--- Js-Basics/palindrome.js
+++ Js-Basics/palindrome.js
@@ -43,3 +43,13 @@ let palindromme = function(word) {
     return (start === flip);
 }
 console.log(palindromme("reviver")) 
+
+let palindromes = function(word) {
+    let len = word.length;
+    let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
+    let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
+    let flip = end.split('').reverse().join('');
+    return (start === flip);
+}
+
+console.log(palindromes('reviver'));
