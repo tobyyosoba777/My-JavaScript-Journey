3e0439cb4bb8824f04a0f7fcd4815d0b6281e25e
tobiloba <tobiosoba777@gmail.com>
2023-10-27 10:15:59 -0700

    Updated

--- Js-Basics/loops.js
+++ Js-Basics/loops.js
@@ -155,7 +155,7 @@ for (i=0; i<arrayy.length; i++) {
 }
 console.log(total);
 
-var add1 = [1,2,3,4,5,6,7,8,89,0];
+var add1 = [1,2,3,4,5,6,7,8,89,0,209];
 var total = 0;
 
 for(i=0; i<add1.length; i++) {
