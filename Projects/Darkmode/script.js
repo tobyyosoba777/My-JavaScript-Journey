d03e68e21dbeb372f3621816364703acc0174776
tobiloba <tobiosoba777@gmail.com>
2023-10-30 13:59:24 -0700

    Updated

---
+++ Projects/Darkmode/script.js
@@ -0,0 +1,15 @@
+let darkmode = document.querySelector('#darkmode');
+
+darkmode.onclick = () =>  {
+    if(darkmode.classList.contains("ri-moon-fill")) {
+        darkmode.classList.replace("ri-moon-fill", "ri-sun-fill");
+        document.body.classList.add("dark");
+    }
+    else {
+        darkmode.classList.replace("ri-sun-fill", "ri-moon-fill");
+        document.body.classList.remove("dark")
+    }
+}
+
+
+
