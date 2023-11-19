8268fc7ef2d1330f6592ecead47324fcfb2b54eb
tobiloba <tobiosoba777@gmail.com>
2023-11-19 08:32:07 -0800

    Updated

--- Projects/palindrome1/script.js
+++ Projects/palindrome1/script.js
@@ -23,3 +23,28 @@ function palindrome() {
         resultElement.textContent = "Not a Palindrome"
     }
 }
+
+function palindromeee() {
+    let word = document.getElementById('word').value.toLowerCase();
+    
+    function check(word) {
+        let len = word.length;
+        let start = word.substring(0, Math.floor(len / 2));
+        let end = word.substring(len - Math.floor(len / 2));
+        let flip = end.split('').reverse().join('');
+        if (start === flip) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    let resultElement = document.getElementById('result');
+    if (check(word)) {
+        resultElement.textContent = "String is a plaindrome";
+    }
+    else {
+        resultElement.textContent = "String is not a palindrome"
+    }
+}
