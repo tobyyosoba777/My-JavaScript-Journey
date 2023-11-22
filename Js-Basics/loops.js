48f327e0e15dd0679745f5125ffdd019564b0304
tobiloba <tobiosoba777@gmail.com>
2023-11-22 05:55:42 -0800

    Updated

--- Js-Basics/loops.js
+++ Js-Basics/loops.js
@@ -261,4 +261,30 @@ function addingg() {
 console.log(typeof newArray)
 
 //Nested for Loops 
+function multiplyAll(arr) {
+    var product = 1;
 
+    for(var i = 0; i < arr.length; i++) {
+        for(j = 0; j < arr[i].length; j++) {
+            product *= arr[i][j];
+        }
+    }
+    return product
+}
+
+var product = multiplyAll([[1,2], [3,4], [5,6,7]]);
+console.log(product);
+
+
+function multiply(arr) {
+    var product = 1;
+
+    for(i = 0; i < arr.length; i++) {
+        for(j = 0; j < arr[i].length; j++) {
+            product *= arr[i][j];
+        }
+    }
+    return product
+}
+product = multiply([[1,2], [3,4], [5,6,7]]);
+console.log(product);
