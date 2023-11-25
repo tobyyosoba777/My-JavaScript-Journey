f7022fb1cfa6fc0a10eda7a148ae0883eb4fe799
tobiloba <tobiosoba777@gmail.com>
2023-11-25 14:19:53 -0800

    Updated

--- Js-Basics/loops.js
+++ Js-Basics/loops.js
@@ -329,3 +329,17 @@ function multi(arr) {
 }
 product = multi([[1,2,3], [4,5,6], [7,8,9,10,12]])
 console.log(product);
+
+
+function mal(arr) {
+    product = 1;
+
+    for (i=0; i<arr.length; i++) {
+        for(j=0; j<arr[i].length; j++) {
+            product *= arr[i][j];
+        }
+    }
+    return product;
+}
+product = mal([[1,2,3] ,[4,5], [10, 10]]);
+console.log(product);
