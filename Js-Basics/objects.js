3d95e9488dc66cb3234e68c4098b3014337b9a0e
tobiloba <tobiosoba777@gmail.com>
2023-11-26 13:56:17 -0800

    Updated

--- Js-Basics/objects.js
+++ Js-Basics/objects.js
@@ -541,3 +541,54 @@ function updateRecords(id, prop, value) {
 // updateRecords(5439, "artist", "ABBA")
 console.log(updateRecords(5439, "artist", "ABband"));
 
+
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "Unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+]
+
+function lookUpProfile(name, prop) {
+    for(var i = 0; i<contacts.length; i++) {
+        if (contacts[i].firstName === name) {
+            return contacts[i][prop] || "No Such Property";
+        }
+    }
+    return "No Such Content"
+}
+
+data = lookUpProfile("Akira", "likes");
+console.log(data);
+
+
+function lookUpProfilee(name, prop) {
+    for(var i = 0; i<contacts.length; i++) {
+        if(contacts[i].firstName === name) {
+            return contacts[i][prop] || "No Such Property exists";
+        }
+    }
+    return name + " Not Found In Contacts";
+}
+var newData = lookUpProfilee("tobi", "firstname");
+console.log(newData);
