2f9c19c4583e200d0f3c0d0249acb70160e8c6cd
tobiloba <tobiosoba777@gmail.com>
2023-11-26 16:46:41 -0800

    Updated

--- Js-Basics/main.js
+++ Js-Basics/main.js
@@ -19,3 +19,15 @@ function randNum() {
     return Math.floor(Math.random() * 1000);
 }
 console.log(randNum());
+
+function randRange(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+console.log(randRange(1, 10));
+
+function newRange(myMax, myMin) {
+
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+}
+console.log(newRange(0,40));
