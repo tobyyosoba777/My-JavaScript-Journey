96925b30719459565c385f3457e993b41bab1761
tobiloba <tobiosoba777@gmail.com>
2023-12-05 10:16:47 -0800

    Updated

--- ES6/IIFE.js
+++ ES6/IIFE.js
@@ -1,7 +1,10 @@
 //IIFE - Immediately Invoked Function Expression
 
 function doSomething() {
-    //function declaration
+    //function Declaration
+}
+var somethingElse = function() {
+    //function Expression
 }
 
 //Over here the function 'firstword' is what we refer to as an anonymous function and it is not called directly
@@ -14,3 +17,15 @@ logword("Hello world");
 (function word(word) {
     console.log(word)
 })('Hello i am testing IIFE');
+
+var something = (function (word) {
+    console.log(word);
+})('HMM IIFE') //This automatically execute without having to call the function because it is immediately invoked funtion (IIFE);
+
+var testing = (function() {
+    return {
+        "name": "Tobiloba",
+        "age": 19,
+        "id": 200129034
+    }
+})();
