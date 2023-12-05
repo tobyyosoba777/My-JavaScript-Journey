b2b4bb79061bc3ff97769363246a0bf58746b312
tobiloba <tobiosoba777@gmail.com>
2023-12-05 09:59:19 -0800

    Updated

--- ES6/IIFE.js
+++ ES6/IIFE.js
@@ -1,6 +1,12 @@
 //IIFE Stands for Immediately Invoked Function Expression
 
-var logword = function firstword(word) {
+//Over here the function 'firstword' is what we refer to as an anonymous function and it is not called directly
+var logword = function (word) {
     console.log(word);
 }
 logword("Hello world");
+
+//How to immediately invoke your function expression
+(function word(word) {
+    console.log(word)
+})('Hello i am testing IIFE');
