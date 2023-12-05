a2a91c96091106a962deec1c5fd3860a2c2a18cf
tobiloba <tobiosoba777@gmail.com>
2023-12-05 11:29:32 -0800

    UPdated

--- ES6/IIFE.js
+++ ES6/IIFE.js
@@ -134,9 +134,4 @@ const increment = (function() {
 increment(20, 5);
 
 //Another Practice
-const increment1 = (function() {
-    return function increment(number, value) {
-        return `${number + value}`
-    }
-})()
-console.log(increment1(10, 53))
+const increment1
