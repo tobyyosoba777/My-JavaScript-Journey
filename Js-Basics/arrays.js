1a26581874fff6daf889ea3b2b1ce114e0b0d1a9
tobiloba <tobiosoba777@gmail.com>
2023-12-18 13:21:22 -0800

    Updated

--- Js-Basics/arrays.js
+++ Js-Basics/arrays.js
@@ -167,5 +167,17 @@ let a = 8, b = 6;
 (() => {
     "Use Strict";
     [a, b] = [b, a];
-})()
-console.log(a)
+})();
+console.log(a);
+console.log(b);
+
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+
+function removeFirstTwo(list) {
+    const [ , , ...arr] = list;
+    return arr;
+}
+const arr = removeFirstTwo(source)
+console.log(arr);
+console.log(source);
