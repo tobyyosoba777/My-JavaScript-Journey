a7b05fb85f1dc0099c298199fe8be5b76a84947f
tobiloba <tobiosoba777@gmail.com>
2023-12-26 06:43:16 -0800

    Updated

--- DOM(Document Object Model)/temp.js
+++ DOM(Document Object Model)/temp.js
@@ -1,22 +1,24 @@
 //Selecting Elements
-let id = document.getElementById("id");
-document.querySelector(".selector"); //Returns the first element that matches the specified CSS selector.
-document.querySelectorAll(".selectors") //Returns all nodes that matches the specified CSS selector.
-document.getElementsByTagName("div") //Rteurns nodeList of div elements
+let id = document.getElementById("id"); 
+let selector = document.querySelector(".selector"); //Returns the first element that matches the CSS selector.
+let selectors = document.querySelectorAll(".selectors") //Returns nodes that matches the CSS selector.
+let divs = document.getElementsByTagName("div") //Returns nodeList of div elements
 
 //Creating Elements
 let createdElement = document.createElement("p");
+
 //setting style to createdElement
 createdElement.style.color = "#fff";
 
 //Manipulating Elements
-let parentElement = document.createElement("div"); //Create a parent div
-parentElement.appendChild(createdElement); //Sets this as the child element
+let parentElement = document.createElement("div");  //Create a parent div
+parentElement.appendChild(createdElement);         //Sets this as the child element
+parentElement.textContent = "Added Text";         //Sets this as the content in the div tag
 
-element.classList.add(className); //Adds a class to an element.
-element.classList.remove(className); //Removes a class from an element.
-element.classList.toggle(className); //Toggles the presence of a class on an element.
+element.classList.add(className);           //Adds a class to an element.
+element.classList.remove(className);        //Removes a class from an element.
+element.classList.toggle(className);        //Toggles the presence of a class on an element.
 
 
-element.addEventListener(event, handler);  //Attaches an event handler to an element.
+element.addEventListener(event, handler);      //Attaches an event handler to an element.
 element.removeEventListener(event, handler);  //Removes an event handler from an element.
