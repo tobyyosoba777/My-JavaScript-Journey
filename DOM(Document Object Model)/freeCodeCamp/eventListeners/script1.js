7b0c7729ae16cdb46a62cedaca44415fae378a77
tobiloba <tobiosoba777@gmail.com>
2023-12-26 17:16:13 -0800

    Updated

--- DOM(Document Object Model)/freeCodeCamp/eventListeners/script1.js
+++ DOM(Document Object Model)/freeCodeCamp/eventListeners/script1.js
@@ -45,6 +45,11 @@ let hiddenContent = document.querySelector('.hidden-content');
 
 revealBtn.addEventListener("click", () => {
     if(hiddenContent.classList.contains('reveal-btn')) {
-        hiddenContent.classList.remove('reveal-btn')
+        hiddenContent.classList.remove('reveal-btn');
+        revealBtn.textContent = "Reveal More"
+    }
+    else {
+        hiddenContent.classList.add('reveal-btn');
+        revealBtn.textContent = "Reveal More"
     }
 })
