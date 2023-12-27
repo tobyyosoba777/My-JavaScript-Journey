b73376f6c152e80d600c4f665511ccd53ab7d418
tobiloba <tobiosoba777@gmail.com>
2023-12-26 17:13:13 -0800

    UPdated

--- DOM(Document Object Model)/freeCodeCamp/eventListeners/script1.js
+++ DOM(Document Object Model)/freeCodeCamp/eventListeners/script1.js
@@ -26,16 +26,25 @@
 //     }
 // })
 
-let revealbtn = document.querySelector(".reveal-btn");
-let hiddenContent = document.querySelector(".hidden-content")
+// let revealbtn = document.querySelector(".reveal-btn");
+// let hiddenContent = document.querySelector(".hidden-content")
 
-revealbtn.addEventListener("click", () => {
-    if(hiddenContent.classList.contains("reveal-btn")) {
-        hiddenContent.classList.remove("reveal-btn");
-        revealbtn.textContent = "Reveal More";
-    }
-    else {
-        hiddenContent.classList.add("reveal-btn");
-        revealbtn.textContent = "Hide All";
+// revealbtn.addEventListener("click", () => {
+//     if(hiddenContent.classList.contains("reveal-btn")) {
+//         hiddenContent.classList.remove("reveal-btn");
+//         revealbtn.textContent = "Reveal More";
+//     }
+//     else {
+//         hiddenContent.classList.add("reveal-btn");
+//         revealbtn.textContent = "Hide All";
+//     }
+// })
+
+let revealBtn = document.querySelector('.reveal-btn');
+let hiddenContent = document.querySelector('.hidden-content');
+
+revealBtn.addEventListener("click", () => {
+    if(hiddenContent.classList.contains('reveal-btn')) {
+        hiddenContent.classList.remove('reveal-btn')
     }
 })
