b800ab4622463f07b49ee8c3cddadacc9a24ac24
tobiloba <tobiosoba777@gmail.com>
2023-12-27 17:05:04 -0800

    Updated

--- DOM(Document Object Model)/freeCodeCamp/eventDelegation/app.js
+++ DOM(Document Object Model)/freeCodeCamp/eventDelegation/app.js
@@ -2,11 +2,31 @@
 
 //Event Delegation
 
-//
+//This allows us to append a single eventListener to a single parent element that
+//adds the effect to all of its present and future descendants that match a selector
 
-document.getElementById("sports").addEventListener("click", (e) => {
-    if(e.target.tagName == "li") {
-        console.log("clicked on ", e.textContent)
+// document.getElementById("sports").addEventListener("click", (e) => {
+//     if(e.target.tagName == "li") {
+//         console.log("clicked on ", e.textContent)
+//     }
+//     console.log("You clicked on: ", e.target.textContent)
+// })
+
+let sportsBar = document.querySelector('#sports');
+sportsBar.addEventListener("click", (e) => {
+    if(e.target.tagName === 'LI') {
+        e.target.style.backgroundColor = "#fff";
+        e.target.style.color = "#333";
+        console.log(e.target.textContent);
+        //or
+        console.log(e.target.getAttribute('id') + ' is clicked');
     }
-    console.log(e.target.innerHtml)
-})
+});
+
+const sports = document.querySelector("#sports");
+const newSport = document.createElement('li');
+
+newSport.innerText = 'Rugby'
+newSport.setAttribute('id', 'rugby')
+
+sports.appendChild(newSport);
