27f608b637dc04f110d6798c308f9cb8224800dc
tobiloba <tobiosoba777@gmail.com>
2023-12-28 08:45:44 -0800

    Updated

--- DOM(Document Object Model)/freeCodeCamp/quoteGenerator/app.js
+++ DOM(Document Object Model)/freeCodeCamp/quoteGenerator/app.js
@@ -1,3 +1,25 @@
 let btn = document.querySelector('#new-quote');
 let quote = document.querySelector('.quote');
-let person = document
+let person = document.querySelector('.person');
+
+const quotes = [
+    {
+        quote: `"The best way to find yourself is to lose yourself 
+        in the service of others"`,
+        person: `Mahatma Gandhi`
+    },
+
+    {
+        quote: `"If you want to live a happy life, tie it to a goal not  
+        to people or other things. "`,
+        person: `Albert Einstein`
+    },
+
+    {
+        quote: `"At his best, man is the noblest of all animals; separated 
+        from law and justice he is the worst"`,
+        person: `Aristotle`
+    },
+    
+
+]
