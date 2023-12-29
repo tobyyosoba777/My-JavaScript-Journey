6c3bc45819a5b8b6ec997a5922875e5f1a366a1d
tobiloba <tobiosoba777@gmail.com>
2023-12-29 10:30:22 -0800

    UPdated

--- DOM(Document Object Model)/freeCodeCamp/quoteGenerator/practice/app.js
+++ DOM(Document Object Model)/freeCodeCamp/quoteGenerator/practice/app.js
@@ -42,8 +42,69 @@ const quotes = [
       { quote: `"Don't watch the clock; do what it does. Keep going."`, author: "Sam Levenson" },
       { quote: `"You have within you right now, everything you need to deal with whatever the world can throw at you."`, author: "Brian Tracy" },
       { quote: `"What lies behind us and what lies before us are tiny matters compared to what lies within us."`, author: "Ralph Waldo Emerson" },
-      { quote: `"The only limit to our realization of tomorrow will be our doubts of today."`, author: "Unknown"}
+      { quote: `"The only limit to our realization of tomorrow will be our doubts of today."`, author: "Unknown"},
+      { quote: `"The best way to find yourself is to lose yourself 
+        in the service of others. "`,
+        author: `Mahatma Gandhi`
+    },
+
+    {
+        quote: `"If you want to live a happy life, tie it to a goal not  
+        to people or other things. "`,
+        author: `Albert Einstein`
+    },
+
+    {
+        quote: `"At his best, man is the noblest of all animals; separated 
+        from law and justice he is the worst. "`,
+        author: `Aristotle`
+    },
+
+    {
+        quote: `"Your time is limited so dont waste it living someone 
+        else's life. "`,
+        author: `Steve Jobs`
+    },
+
+    {
+        quote: `"Tell me and i forget. Teach me and i remember. Involve me 
+        and i learn. "`,
+        author: `Benjamin Franklin`
+    },
+
+    {
+        quote: `"If you look at what you have in life, you'll always have 
+        more. If you look at what you don't have in life, you'll never have 
+        enough. "`,
+        author: `Oprah Winfrey`
+    },
+
+    {
+        quote: `"It does not matter how slowly you go as long as you do not
+        stop. "`,
+        author: `confucius`
+    },
+
+    {
+        quote: `"Our lives begin to end the day we become silent about the 
+        things that matter"`,
+        author: `Martin Luther King, Jr. `
+    },
+
+    {
+        quote: `"Remember that not getting what you want is sometimes a 
+        wonderful stroke of luck"`,
+        author: `Dalai Lama`
+    },
+
+    {
+        quote: `"The journey of a thousand miles begin with one step. "`,
+        author: `Lao Tzu`
+    }
     ]
+
+    
+
 
 const bgImages = [
     {
