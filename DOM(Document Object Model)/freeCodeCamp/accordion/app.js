0054e4dcded4da0f84ae869344bc992f410587a2
tobiloba <tobiosoba777@gmail.com>
2023-12-31 06:43:35 -0800

    Updated

--- DOM(Document Object Model)/freeCodeCamp/accordion/app.js
+++ DOM(Document Object Model)/freeCodeCamp/accordion/app.js
@@ -1,2 +1,7 @@
 let accordion = document.getElementsByClassName('content-container');
 
+for(i = 0; i<accordion.length; i++) {
+    accordion[i].addEventListener("click", function() {
+        this.classList.toggle('active');
+    })
+}
