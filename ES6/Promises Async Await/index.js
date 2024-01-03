96580c8fedf0d52287cd598f08e4b78a5519e41a
tobiloba <tobiosoba777@gmail.com>
2024-01-03 10:53:31 -0800

    Update

---
+++ ES6/Promises Async Await/index.js
@@ -0,0 +1,17 @@
+// const axiosRequest = require('axios');
+
+// let response = axiosRequest.get('https://www.boredapi.com/api/activity');
+// console.log(`you could ${response.data.activity}`);
+
+import axios from 'axios';
+
+async function fetchData() {
+    try {
+        const response = await axios.get('https://www.boredapi.com/api/activity');
+        console.log(`You could ${response.data}`);
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+    }
+}
+
+fetchData();
