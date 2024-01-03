8f2c9446e60490b50bd56e40c09407ea925107e3
tobiloba <tobiosoba777@gmail.com>
2024-01-03 05:24:51 -0800

    I'm a fucking genius haahaha

--- DOM(Document Object Model)/freeCodeCamp/Todo-List/script2.js
+++ DOM(Document Object Model)/freeCodeCamp/Todo-List/script2.js
@@ -8,4 +8,40 @@ addTask.addEventListener("click", () => {
 
     let li = document.createElement('li');
     li.innerText = `${inputTask.value}`;
+    task.appendChild(li);
+
+    let checkBtn = document.createElement('button');
+    checkBtn.innerHTML = `<i class = "fa-solid fa-check"></i>`;
+    checkBtn.classList.add('checkTask');
+    task.appendChild(checkBtn);
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML = `<i class = "fa-solid fa-trash-can"></i>`;
+    deleteBtn.classList.add('deleteTask');
+    task.appendChild(deleteBtn);
+
+    if(inputTask.value === "") {
+        alert("Cannot be Blank");
+    }
+    else {
+        taskContainer.appendChild(task);
+    }
+
+    inputTask.value = "";
+
+    checkBtn.addEventListener("click", () => {
+        if(li.style.textDecoration === "none") {
+            li.style.textDecoration = "line-through"
+        }
+        else {
+            li.style.textDecoration = "none"
+        }
+    })
+
+    deleteBtn.addEventListener("click", (e) => {
+        e.stopPropagation()
+        let target = e.target;
+        target.parentElement.remove();
+    })
+
 })
