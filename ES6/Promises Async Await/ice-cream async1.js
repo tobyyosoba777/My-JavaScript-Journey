ebd2f2c91979f98fad0b6cf119db7ead77fc17b5
tobiloba <tobiosoba777@gmail.com>
2024-01-04 10:49:57 -0800

    Updated

--- ES6/Promises Async Await/ice-cream async1.js
+++ ES6/Promises Async Await/ice-cream async1.js
@@ -5,26 +5,25 @@ let stocks = {
     toppings: ["chocolate", "peanuts"]
 };
 
-
 let order = (fruit_name, call_production) => {
     setTimeout(() => {
         console.log(`${stocks.fruits[fruit_name]} was selected`);
-        call_production();
     }, 2000);
+    call_production()
 }
 
 let production = () => {
     setTimeout(() => {
-        console.log("Production has started");
+        console.log("production has started");
 
         setTimeout(() => {
-            console.log("The fruit has been chopped");
-            setTimeout(() => {
-                console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`)
+            console.log(`The fruit has been chopped`)
 
+            setTimeout(() => {
+                console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
             }, 1000);
-        }, 2000)
+        }, 2000);
     }, 0);
 }
 
-order("2", production); 
+order("1", production)
