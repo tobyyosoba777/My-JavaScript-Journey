0c27145f35993e66dabb41d91318daeaf19863b9
tobiloba <tobiosoba777@gmail.com>
2024-01-20 05:14:37 -0800

    Updated

--- ES6/Promises Async Await/Traversy Media/promises.js
+++ ES6/Promises Async Await/Traversy Media/promises.js
@@ -21,7 +21,7 @@ let createPost = (post) => {
         setTimeout(() => {
             posts.push(post)
 
-            const error = false;
+            const error = true;
 
             if(!error) {
                 resolve();
@@ -32,4 +32,19 @@ let createPost = (post) => {
     })
 }
 
-createPost({ title: 'Post Three', body: "This is post Three" }).then(getPosts);
+// createPost({ title: 'Post Three', body: "This is post Three" })
+// .then(getPosts)
+// .catch(err => console.log(err))
+
+// Promise.all
+
+const promise1 = Promise.resolve("Hello World")
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => 
+setTimeout(resolve, 2000, 'Goodbye'));
+
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+
+Promise.all([promise1, promise2, promise3, promise4]).then((
+    (values) => console.log(values)
+))
