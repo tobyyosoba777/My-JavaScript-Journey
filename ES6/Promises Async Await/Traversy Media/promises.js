e04a0e83e7b356b5aa442750b795325dd3bf4e6d
tobiloba <tobiosoba777@gmail.com>
2024-01-20 02:37:14 -0800

    Updated

---
+++ ES6/Promises Async Await/Traversy Media/promises.js
@@ -0,0 +1,35 @@
+const posts = [
+    {title: 'Post One', body: 'This is post One'},
+    {title: 'Post Two', body: 'This is post two'}
+];
+
+const getPosts = () => {
+    setTimeout(() => {
+       let output = '';
+       let bodyy = '';
+       posts.forEach((post) => {
+        output += `<li>${post.title}</li>`
+        bodyy += `<p>${post.body}</p>`
+       }) 
+       document.body.innerHTML = output;
+    }, 1000);
+}
+getPosts()
+
+let createPost = (post) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post)
+
+            const error = false;
+
+            if(!error) {
+                resolve()
+            } else {
+                reject('Error:something went wrong')
+            }
+         }, 2000);
+    })
+}
+
+createPost({ title: 'Post Three', body: "This is post Three" })
