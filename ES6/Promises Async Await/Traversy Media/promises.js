a991799e27624cf970698b5c815ca286c0b6b303
tobiloba <tobiosoba777@gmail.com>
2024-01-21 07:06:50 -0800

    Updated

--- ES6/Promises Async Await/Traversy Media/promises.js
+++ ES6/Promises Async Await/Traversy Media/promises.js
@@ -65,3 +65,10 @@ const fetchUsers = async () => {
 }
 
 fetchUsers();
+
+const fetcUsers = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    console.log(data);
+}
+fetcUsers()
