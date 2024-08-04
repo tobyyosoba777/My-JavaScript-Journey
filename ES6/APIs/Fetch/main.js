e7f8275da74c3227dc4b38add782802d74118929
tobiloba <tobiosoba777@gmail.com>
2024-08-04 06:56:32 -0700

    Updated

---
+++ ES6/APIs/Fetch/main.js
@@ -0,0 +1,6 @@
+const route = "posts"
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error("Error:", error))
